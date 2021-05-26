3649ba1004fe35c1409676f43374fd0b57b167e2
Ciontuclaudia00 <81517815+Ciontuclaudia00@users.noreply.github.com>
2021-05-26 22:38:19 +0300

    Add files via upload

---
+++ Roll-A-Ball/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    private Rigidbody rb;
+    private int count;
+    private float movementX;
+    private float movementY;
+   // private float jump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 44)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Vector3 jump = new Vector3(0.0f, 300.0f, 0.0f);
+            GetComponent<Rigidbody>().AddForce(jump);
+        }
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
+        }
+    }
+}
